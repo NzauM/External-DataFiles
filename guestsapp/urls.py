e419e63a9eddd270d4999646d2c5a92193ff2c74
NzauM <mercywaenu16@gmail.com>
2022-03-15 13:17:22 +0300

    configure for deployment

--- guestsapp/urls.py
+++ guestsapp/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-    path("", views.welcome, name = 'welcome'),
-    path("listusers",views.listUsers, name="listusers"),
+    path("",views.listUsers, name="listusers"),
     path("showguest/<int:id>",views.getGuest, name="showguest" ),
     path("newguest", views.guestForm, name="guestForm"),
     path("updateguest/<int:id>", views.updateGuest, name="updateguest"),
--- guestsproject/settings.py
+++ guestsproject/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 # from pathlib import Path
 # import dj_database_url
 import os
-import django_heroku
+# import django_heroku
 import dj_database_url
 
 
@@ -144,3 +144,5 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# django_heroku.settings(locals())
